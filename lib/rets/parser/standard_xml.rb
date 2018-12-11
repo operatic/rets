a59d694bb2c4f98c396bbd1a5776ccce77bd559a
justin bates <justin@parallelmade.com>
2018-12-11 16:08:31 -0500

    Updates to get parse more collections

--- lib/rets/parser/standard_xml.rb
+++ lib/rets/parser/standard_xml.rb
@@ -14,80 +14,67 @@ module Rets
             end
           end
         end
-
         properties
       end
 
       def self.xml_to_hash(xml, hash={})
-        hash ||= {}
+        if xml.class == Nokogiri::XML::Element
+          hash = set_attributes(xml, hash)
+          return get_collection xml if collections.include? xml.name
+        end
 
-        if xml.class != Nokogiri::XML::NodeSet && xml.name == 'PropertyDetails'
-          hash = set_attributes(hash, xml)
-
-          xml.children.each do |child|
-            if child.children.length == 1 && child.children[0].type == Nokogiri::XML::Reader::TYPE_TEXT
-              hash[child.name] = set_attributes({}, child)
-              hash[child.name] = child.children[0].text
+        xml.children.each do |child|
+          if child.children.length == 1 && child.children[0].type == Nokogiri::XML::Reader::TYPE_TEXT
+            hash[child.name] = child.children.first.text
+          else
+            if child.class == Nokogiri::XML::Element
+              hash[child.name] = set_attributes(child, {})
+              hash[child.name] = self.xml_to_hash(child, hash[child.name])
             else
-              if child.name == 'Photo'
-                hash["#{child.name}s"] = set_attributes({}, child)
-                hash["#{child.name}s"] = get_photos(child) if child.name == "Photo"
-              else
-                hash[child.name] = set_attributes({}, child)
-                hash[child.name] = self.xml_to_hash(child.children, hash[child.name])
-              end
-            end
-          end
-        elsif xml.class == Nokogiri::XML::NodeSet
-          xml.each do |node|
-            if node.children.length == 1 && node.children[0].type == Nokogiri::XML::Reader::TYPE_TEXT
-              hash[node.name] = set_attributes({}, node)
-              hash[node.name] = node.children[0].text
-            else
-              if node.name == 'Phones'
-                hash[node.name] = []
-
-                node.children.each do |child|
-                  hash[node.name] << get_phone(child) if node.name == "Phones"
-                end
-              else
-                hash[node.name] = set_attributes({}, node)
-                hash[node.name] = self.xml_to_hash(node.children, hash[node.name])
+              child.children.each do |node|
+                hash[node.name] = set_attributes(node, {})
+                hash[node.name] = self.xml_to_hash(node, {})
               end
             end
           end
         end
-
         hash
       end
 
-      def self.set_attributes(hash, xml)
-        if xml.attributes.length > 0
-          xml.attributes.each do |attribute|
-            hash[attribute[1].name] = attribute[1].value
+      def self.set_attributes(xml, hash)
+        if xml.attribute_nodes.length > 0
+          xml.attribute_nodes.each do |attribute|
+            hash[attribute.name] = attribute.value
           end
-
-          hash
         end
-      end
-
-      def self.get_phone(phone)
-        hash = {}
-        hash[phone.attributes["PhoneType"].value] = phone.text
         hash
       end
 
-      def self.get_photos(child)
-        photos = []
-        child.children.each do |photo|
-          hash = {}
-          photo.children.each do |attribute|
-            hash[attribute.name] = attribute.text
-          end
-          photos << hash
-        end
+      def self.collections
+        [
+          "Phones",
+          "Rooms",
+          "UtilitiesAvailable",
+          "Websites",
+          "OpenHouse",
+          "ParkingSpaces",
+          "Photo",
+          "Specialties",
+          "Designations",
+          "Languages"
+        ]
+      end
 
-        photos
+      def self.get_collection(xml)
+        collection = []
+        xml.children.each do |node|
+          hash = set_attributes node, {}
+          node.children.each do |item|
+            hash[item.name] = item.text
+          end
+          collection << hash
+        end
+        collection
       end
     end
   end
