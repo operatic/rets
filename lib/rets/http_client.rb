0744a080e9f2dfab0583229664cf3f8f846d35ed
Kyle <69905437+kyle-operatic@users.noreply.github.com>
2021-10-05 11:03:32 -0400

    Override HTTPClient to use system's SSL certificates

--- lib/rets/http_client.rb
+++ lib/rets/http_client.rb
@@ -1,6 +1,16 @@
 require 'http-cookie'
 require 'httpclient'
 
+class HTTPClient
+  alias original_initialize initialize
+
+  def initialize(*args, &block)
+    original_initialize(*args, &block)
+    # Force use of the default system CA certs (instead of the 6 year old bundled ones)
+    @session_manager&.ssl_config&.set_default_paths
+  end
+end
+
 module Rets
   class HttpClient
     attr_reader :http, :options, :logger, :login_url
